33246b7ec8650b67af043245d5a3c74fcd9ddf5e
Elizabeth Karst <lkarst@outlook.com>
2018-08-21 02:48:24 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,7 @@ class School
 
   def add_student(name, grade)
     if roster.keys.include?(grade)
-      @roster[grade][name] << name
+      @roster[grade] << name
     else
       @roster[grade] = [name]
     end
