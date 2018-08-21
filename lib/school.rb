e649c3c5b1ff3dd9023819e95eda2da2c1418a29
Elizabeth Karst <lkarst@outlook.com>
2018-08-21 02:38:18 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,13 @@
 class School
   attr_accessor :roster
+  attr_reader :add_student
 
   def initialize(school_name)
     @school_name = school_name
     @roster = {}
+  end
+
+  def add_student=(student,num=1)
   end
 
 end
